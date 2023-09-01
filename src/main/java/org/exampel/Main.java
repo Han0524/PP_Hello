897767fc8cc32c5459e7130c239f60d43bbb5fdf
hansanghwa <hsh21900783@handong.ac.kr>
2023-09-01 17:22:50 +0900

    Create Hello Project

---
+++ src/main/java/org/exampel/Main.java
@@ -0,0 +1,10 @@
+package org.exampel;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("hello");
+
+        Student s1 = new Student("21900783", "한상화");
+        System.out.println(s1.toString());
+    }
+}
